f1ffc4d7b25dcb4a36a2d1d7fc5c5aa27047b309
Mateus <31539494+Matuiss2@users.noreply.github.com>
2019-01-31 18:10:03 -0200

    Add __init__ file and preview to what pylint + black do (#2)
    Add missing __init__ file and fix inconsistencies in setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,21 @@
+"""Groups the project overall info and requirements"""
 from os.path import realpath, dirname, join
 from setuptools import setup, find_packages
 
-DISTNAME = 'leelastar'
-DESCRIPTION = 'LeelaStar: Crowd-Sourced Deep Reinforcement Learning Agent for Starcraft 2'
-AUTHOR = 'LeelaStar Developers'
-AUTHOR_EMAIL = 'leelastar.devs@gmail.com'
+
+DISTNAME = "leelastar"
+DESCRIPTION = "LeelaStar: Crowd-Sourced Deep Reinforcement Learning Agent for Starcraft 2"
+AUTHOR = "LeelaStar Developers"
+AUTHOR_EMAIL = "leelastar.devs@gmail.com"
 URL = "https://github.com/leelastar/leelastar-training"
 LICENSE = "Apache License, Version 2.0"
 VERSION = "0.0.1"
 
 PROJECT_ROOT = dirname(realpath(__file__))
-REQUIREMENTS_FILE = join(PROJECT_ROOT, 'requirements.txt')
+REQUIREMENTS_FILE = join(PROJECT_ROOT, "requirements.txt")
 
 with open(REQUIREMENTS_FILE) as f:
-    install_reqs = f.read().splitlines()
+    REQUIREMENTS = f.read().splitlines()
 
 if __name__ == "__main__":
     setup(
@@ -23,8 +25,8 @@ if __name__ == "__main__":
         author=AUTHOR,
         author_email=AUTHOR_EMAIL,
         packages=find_packages(),
-        install_requires=install_reqs,
+        install_requires=REQUIREMENTS,
         include_package_data=True,
         url=URL,
-        license=LICENSE
+        license=LICENSE,
     )
